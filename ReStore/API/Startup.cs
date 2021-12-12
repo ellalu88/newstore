cc9f83f2e786f9803b0ea50f1244547df1854e6a
luchiyu <luchiyu8@gmail.com>
2021-12-12 17:38:39 +1300

    Client basic components

--- ReStore/API/Startup.cs
+++ ReStore/API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
                 // opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
                 opt.UseSqlite("Data source=store.db");
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,10 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(Opt=>{
+                Opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
